5c301f4dc5a0378eac0c8a81e4ff0d3884cc19eb
saoud-ahmed-khan <60979682+saoud-ahmed-khan@users.noreply.github.com>
2020-09-17 22:32:51 +0500

    local storage

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ const obj={
     name:"saoud",
     lname:"khan"
 }
+//to save value in local storage
+localStorage.setItem("name","saoud")
+localStorage.setItem("fname",'mehmood')
+//to get valcu from local storage
+let a=localStorage.getItem("name");
+let b=localStorage.getItem("fname");
+
+console.log(a);
+console.log(b);
+console.log(localStorage.length)
