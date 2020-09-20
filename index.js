38a1141dc67fdfe9bc45374d34565282aebd6d8d
saoud-ahmed-khan <60979682+saoud-ahmed-khan@users.noreply.github.com>
2020-09-20 11:57:22 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,56 +1,58 @@
-const obj = {
-    name: "saoud",
-    lname: "khan"
-}
-//to save value in local storage
-localStorage.setItem("name", "saoud")
-localStorage.setItem("fname", 'mehmood')
-//to get valcu from local storage
-let a = localStorage.getItem("name");
-let b = localStorage.getItem("fname");
-//removing
-// localStorage.removeItem("name")
-// localStorage.removeItem("fname");
+// const obj = {
+//     name: "saoud",
+//     lname: "khan"
+// }
+// //to save value in local storage
+// localStorage.setItem("name", "saoud")
+// localStorage.setItem("fname", 'mehmood')
+// //to get valcu from local storage
+// let a = localStorage.getItem("name");
+// let b = localStorage.getItem("fname");
+// //removing
+// // localStorage.removeItem("name")
+// // localStorage.removeItem("fname");
 
 
-console.log(a);
-console.log(b);
-console.log(localStorage.length)
-//sessional storage
-// 
+// console.log(a);
+// console.log(b);
+// console.log(localStorage.length)
+// //sessional storage
+// // 
 
-let arr = ['saoud', 'kinza', 'farman'];
-let [, secondname] = arr;
-console.log(secondname)
+// let arr = ['saoud', 'kinza', 'farman'];
+// let [, secondname] = arr;
+// console.log(secondname)
 
-let newobject = 
-{
-    name:'saaoud',
-    fname:'mehmood',
-    city:'karachi',
-    town:'orangi',
-    hobies: {
-                sports:'cricket',
-                food:'pizza',
-                dish:'karahi'
-           }
-}
+// // let newobject = 
+// // {
+// //     name:'saaoud',
+// //     fname:'mehmood',
+// //     city:'karachi',
+// //     town:'orangi',
+// //     hobies: {
+// //                 sports:'cricket',
+// //                 food:'pizza',
+// //                 dish:'karahi'
+// //            }
+// // }
 
-let {name}=newobject;
-console.log(name)
-let {hobies}=newobject
-console.log(hobies)
-let{dish}=hobies
-console.log(dish)
-let newobj={
-    name:'saaoud',
-    fname:'mehmood',
-    city:'karachi',
-    town:'orangi',
-}
-let spread=
-{
-    ...newobj,
-    hoby:'cooking'
-}
-console.log(spread)
+// // let {name}=newobject;
+// // console.log(name)
+// // let {hobies}=newobject
+// // console.log(hobies)
+// // let{dish}=hobies
+// // console.log(dish)
+// let newobj={
+//     name:'saaoud',
+//     fname:'mehmood',
+//     city:'karachi',
+//     town:'orangi',
+// }
+// // let spread=
+// // {
+// //     ...newobj,
+// //     hoby:'cooking'
+// // }
+// // console.log(spread)
+// let {name, ...restvalue}= newobj
+// console.log(restvalue)
